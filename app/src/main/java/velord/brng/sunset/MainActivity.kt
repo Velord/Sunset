bc675baa653bf1c0f08b00663e8e47c7a5d83a85
Velord <33905170+Velord@users.noreply.github.com>
2019-11-05 14:39:56 +0300

    Initial commit

---
+++ app/src/main/java/velord/brng/sunset/MainActivity.kt
@@ -0,0 +1,12 @@
+package velord.brng.sunset
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
