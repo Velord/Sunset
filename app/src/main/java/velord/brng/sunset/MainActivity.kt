175e7f18eac7f3c78d2f442290e1006fa61ce644
Velord <33905170+velord@users.noreply.github.com>
2019-11-05 18:38:24 +0300

    Chapter 31: Property Animation. Challenge: add a reflection of the sun in the water is Done !

--- app/src/main/java/velord/brng/sunset/MainActivity.kt
+++ app/src/main/java/velord/brng/sunset/MainActivity.kt
@@ -6,16 +6,20 @@ import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.AccelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.animation.doOnEnd
 import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var sceneView: View
     private lateinit var sunView: View
     private lateinit var skyView: View
+    private lateinit var seaView: View
+    private lateinit var sunReflectionView: View
 
     private val blueSkyColor: Int by lazy {
         ContextCompat.getColor(this, R.color.blue_sky)
@@ -45,6 +49,8 @@ class MainActivity : AppCompatActivity() {
         sceneView = findViewById(R.id.scene)
         sunView = findViewById(R.id.sun)
         skyView = findViewById(R.id.sky)
+        seaView = findViewById(R.id.sea)
+        sunReflectionView = findViewById(R.id.sunReflection)
     }
 
     private fun toNight() {
@@ -56,67 +62,100 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun downAnim() {
-        val sunStartY = sceneView.bottom.toFloat()
-        val sunEndY = sunView.top.toFloat()
+        val sunAnim = getSunAnimDownAnimator()
 
-        val heightAnimator = sunAnimator(sunStartY, sunEndY)
+        val withSunReflectAnim = getSunReflectionDownAnimator()
+        val withSunReflectionHeatAnim = getHeatSunReflectionAnimator()
+        val withSunHeatAnim = getHeatSunAnimator()
+        val withSkyAnim = getNightToSunsetSkyAnimator()
 
-        val with = nightToSunsetSkyAnimator()
+        val beforeSkyAnim = getSunsetToBlueSkyAnimator()
 
-        val before = sunsetToBlueSkyAnimator()
-
-        animationQueue(
-            heightAnimator,
-            with, heatSunAnimator(),
-            beforeAnim = before
+        animationBuilder(
+            sunAnim,
+            withSunReflectAnim, withSunReflectionHeatAnim,
+            withSunHeatAnim, withSkyAnim,
+            beforeAnim = beforeSkyAnim
         )
     }
 
     private fun nightAnim() {
-        val sunStartY = sunView.top.toFloat()
-        val sunEndY = sceneView.bottom.toFloat()
+        val sunAnim = getSunAnimNightAnimator()
 
-        val heightAnimator = sunAnimator(sunStartY, sunEndY)
+        val withSunReflectAnim = getSunReflectionNightAnimator()
+        val withSunReflectionHeatAnim = getHeatSunReflectionAnimator()
+        val withSunHeatAnim = getHeatSunAnimator()
+        val withSkyAnim = getBlueToSunsetSkyAnimator()
 
-        val with = blueToSunsetSkyAnimator()
+        val beforeSkyAnim = getSunsetToNightSkyAnimator()
 
-        val before = sunsetToNightSkyAnimator()
-
-        animationQueue(
-            heightAnimator,
-            with, heatSunAnimator(),
-            beforeAnim = before
+        animationBuilder(
+            sunAnim,
+            withSunReflectAnim, withSunReflectionHeatAnim,
+            withSunHeatAnim, withSkyAnim,
+            beforeAnim = beforeSkyAnim
         )
     }
 
-    private fun blueToSunsetSkyAnimator(duration: Long = 3000) =
-        skyAnimator(blueSkyColor, sunsetSkyColor, duration)
+    private fun getSunAnimNightAnimator(): ObjectAnimator {
+        val sunStartY = sunView.top.toFloat()
+        val sunEndY = sceneView.bottom.toFloat()
+        val view = sunView
+        return sunAnimatorBuilder(view, sunStartY, sunEndY)
+    }
 
-    private fun sunsetToNightSkyAnimator(duration: Long = 3000) =
-        skyAnimator(sunsetSkyColor, nightSkyColor, duration)
+    private fun getSunAnimDownAnimator(): ObjectAnimator {
+        val sunStartY = sceneView.bottom.toFloat()
+        val sunEndY = sunView.top.toFloat()
+        val view = sunView
+        return sunAnimatorBuilder(view, sunStartY, sunEndY)
+    }
 
-    private fun nightToSunsetSkyAnimator(duration: Long = 3000) =
-        skyAnimator(nightSkyColor, sunsetSkyColor, duration)
+    private fun getSunReflectionNightAnimator(): ObjectAnimator {
+        val sunReflectFromY = (sunReflectionView.getHeight()).toFloat()
+        val sunReflectToY = (sunReflectionView.getY() - seaView.getHeight())
+        val view = sunReflectionView
+        return sunAnimatorBuilder(view, sunReflectFromY, sunReflectToY)
+    }
 
-    private fun sunsetToBlueSkyAnimator(duration: Long = 3000) =
-        skyAnimator(sunsetSkyColor, blueSkyColor, duration)
+    private fun getSunReflectionDownAnimator(): ObjectAnimator {
+        val sunReflectFromY = (sunReflectionView.getY() - seaView.getHeight())
+        val sunReflectToY = (sunReflectionView.getHeight()).toFloat()
+        val view = sunReflectionView
+        return sunAnimatorBuilder(view, sunReflectFromY, sunReflectToY)
+    }
 
-    private fun sunAnimator(sunStartY: Float,
-                            sunEndY: Float,
-                            duration: Long = 4000) =
-        ObjectAnimator
-            .ofFloat(sunView, "y", sunStartY,  sunEndY)
-            .setDuration(duration)
-            .apply {
-                interpolator = AccelerateInterpolator()
-            }
+    private fun getBlueToSunsetSkyAnimator(duration: Long = 3000) =
+        skyAnimatorBuilder(blueSkyColor, sunsetSkyColor, duration)
 
-    private fun heatSunAnimator(duration: Long = 800,
-                                scaleX: Float = 1.3f,
-                                scaleY: Float = 1.3f,
-                                repeat: Int = 5) =
+    private fun getSunsetToNightSkyAnimator(duration: Long = 3000) =
+        skyAnimatorBuilder(sunsetSkyColor, nightSkyColor, duration)
+
+    private fun getNightToSunsetSkyAnimator(duration: Long = 3000) =
+        skyAnimatorBuilder(nightSkyColor, sunsetSkyColor, duration)
+
+    private fun getSunsetToBlueSkyAnimator(duration: Long = 3000) =
+        skyAnimatorBuilder(sunsetSkyColor, blueSkyColor, duration)
+
+    private fun getHeatSunReflectionAnimator(duration: Long = 800,
+                                             scaleX: Float = 1.3f,
+                                             scaleY: Float = 1.3f,
+                                             repeat: Int = 5) =
+        sunHeatBuilder(sunReflectionView)
+
+    private fun getHeatSunAnimator(duration: Long = 800,
+                                   scaleX: Float = 1.3f,
+                                   scaleY: Float = 1.3f,
+                                   repeat: Int = 5) =
+        sunHeatBuilder(sunView)
+
+    private fun sunHeatBuilder(view: View,
+                               duration: Long = 800,
+                               scaleX: Float = 1.3f,
+                               scaleY: Float = 1.3f,
+                               repeat: Int = 5) =
         ObjectAnimator.ofPropertyValuesHolder(
-            sunView,
+            view,
             PropertyValuesHolder.ofFloat("scaleX", scaleX),
             PropertyValuesHolder.ofFloat("scaleY", scaleY))
             .setDuration(duration)
@@ -126,15 +165,26 @@ class MainActivity : AppCompatActivity() {
                 interpolator = AccelerateInterpolator()
             }
 
-    private fun skyAnimator(from: Int,
-                            to: Int,
-                            duration: Long = 3000) =
+    private fun sunAnimatorBuilder(view: View,
+                                   sunStartY: Float,
+                                   sunEndY: Float,
+                                   duration: Long = 4000) =
+        ObjectAnimator
+            .ofFloat(view, "y", sunStartY,  sunEndY)
+            .setDuration(duration)
+            .apply {
+                interpolator = AccelerateDecelerateInterpolator()
+            }
+
+    private fun skyAnimatorBuilder(from: Int,
+                                   to: Int,
+                                   duration: Long = 3000) =
         ObjectAnimator
             .ofInt(skyView, "backgroundColor", from, to)
             .setDuration(duration)
             .apply { setEvaluator(ArgbEvaluator()) }
 
-    private fun animationQueue(heightAnimator: ObjectAnimator,
+    private fun animationBuilder(heightAnimator: ObjectAnimator,
                                vararg withAnim: ObjectAnimator,
                                beforeAnim: ObjectAnimator) {
         AnimatorSet().apply {
